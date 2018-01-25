6ca87de9bb31897cbe9332ab811b86780c8a2820
Nicola Ferruzzi <nicola.ferruzzi@gmail.com>
2018-01-25 14:31:42 +0100

    Initial commit

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SwiftDAG' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SwiftDAG
+  pod 'Sourcery'
+
+  target 'SwiftDAGTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ SwiftDAG/SwiftDAG.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDAG.h
+//  SwiftDAG
+//
+//  Created by Nicola Ferruzzi on 25/01/2018.
+//  Copyright © 2018 Nicola Ferruzzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftDAG.
+FOUNDATION_EXPORT double SwiftDAGVersionNumber;
+
+//! Project version string for SwiftDAG.
+FOUNDATION_EXPORT const unsigned char SwiftDAGVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDAG/PublicHeader.h>
+
+
